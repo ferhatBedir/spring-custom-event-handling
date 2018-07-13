0042d6e09280b4ba7cf17480f8edaca6fc4408a2
ferhat <ferhat.bedir@hotmail.com>
2018-07-13 12:54:39 +0300

    updated security and swagger classes.

--- src/main/java/com/ferhat/pubsub/config/SecurityConfig.java
+++ src/main/java/com/ferhat/pubsub/config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.ferhat.pubsub.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -22,17 +24,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.anonymous().and().authorizeRequests()
-                .antMatchers("/*", "/public/**", "/v2/**", "/swagger/**").permitAll()
-                .antMatchers("/user/**", "/department/**").authenticated();
-
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.
+                anonymous().and().authorizeRequests()
+                .antMatchers("/v2/**", "/swagger-ui.html").permitAll()
+                .antMatchers("/user/**", "/department/**").authenticated().and().httpBasic();
     }
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         String encodedPassword = passwordEncoder.encode("123123");
-        auth.inMemoryAuthentication().withUser("ferhat").password(encodedPassword).roles("USER");
+        auth.inMemoryAuthentication().withUser("ferhat").password(encodedPassword).roles("User");
     }
 
 }
--- src/main/java/com/ferhat/pubsub/config/SwaggerConfig.java
+++ src/main/java/com/ferhat/pubsub/config/SwaggerConfig.java
@@ -2,20 +2,25 @@ package com.ferhat.pubsub.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
-    @Bean
+    /*@Bean
     public Docket pubSubApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select().apis(RequestHandlerSelectors.basePackage("com.ferhat.pubsub")).build();
-    }
+    }*/
 
     /*
     return new Docket(DocumentationType.SWAGGER_2)
@@ -35,4 +40,17 @@ public class SwaggerConfig {
     /*
 	https://dzone.com/articles/spring-boot-restful-api-documentation-with-swagger swagger info
 	 */
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select().apis(RequestHandlerSelectors.any()).paths(PathSelectors.any()).build().apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo("PubSub Rest Api", "PubSub BackEnd App", "1.0.0",
+                null,
+                new Contact("Ferhat Bedir", "www.tmod.com.tr", "ferhat.bedir@hotmail.com"),
+                null, null, Collections.emptyList());
+    }
 }
